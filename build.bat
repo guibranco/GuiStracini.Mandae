573e1c1435e0fce117c6664a93b5d64e74db96d2
guibranco <gui.branco@hotmail.com.br>
2017-09-28 13:34:41 -0300

    Updated NuGet packages to .NET 4.7

--- build.bat
+++ build.bat
@@ -18,6 +18,6 @@ if "%nuget%" == "" (
 
 mkdir Build
 mkdir Build\lib
-mkdir Build\lib\net40
+mkdir Build\lib\net47
 
 %nuget% pack "src\GuiStracini.Mandae.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
