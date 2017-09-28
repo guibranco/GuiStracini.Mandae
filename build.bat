d4b0bf365d80f4a33a114e5887edfc946a353b56
guibranco <gui.branco@hotmail.com.br>
2017-09-28 14:24:49 -0300

    Added restore command to build.bat

--- build.bat
+++ build.bat
@@ -9,10 +9,12 @@ if not "%PackageVersion%" == "" (
    set version=%PackageVersion%
 )
 
-set nuget=
+set nuget=.nuget\nuget.exe
 if "%nuget%" == "" (
 	set nuget=nuget
 )
+
+%nuget% restore ./
 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild GuiStracini.Mandae.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
