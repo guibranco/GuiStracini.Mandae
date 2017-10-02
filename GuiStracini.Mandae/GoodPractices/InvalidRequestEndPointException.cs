3ac0047b19a06e59970174f1c70eec41ab8a9d2d
guibranco <gui.branco@hotmail.com.br>
2017-10-02 19:19:28 -0300

    Finalized OrderBuilder class.

--- GuiStracini.Mandae/GoodPractices/InvalidRequestEndPointException.cs
+++ GuiStracini.Mandae/GoodPractices/InvalidRequestEndPointException.cs
@@ -16,12 +16,15 @@ namespace GuiStracini.Mandae.GoodPractices
 {
 
     using System;
+    using System.Runtime.Serialization;
 
     /// <summary>
     /// Throws when a invalid request end point pattern is found while resolving the request endpoint
     /// </summary>
     /// <seealso cref="System.Exception" />
-    public sealed class InvalidRequestEndPointException : Exception
+
+    [Serializable]
+    public class InvalidRequestEndPointException : Exception
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="InvalidRequestEndPointException"/> class.
@@ -30,6 +33,14 @@ namespace GuiStracini.Mandae.GoodPractices
         /// <param name="endpointResolved">The endpoint resolved.</param>
         public InvalidRequestEndPointException(String endpointPattern, String endpointResolved)
             : base($"The endpoint {endpointResolved} is not valid for the pattern {endpointPattern}")
+        { }
+
+        /// <summary>Initializes a new instance of the <see cref="T:System.Exception" /> class with serialized data.</summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo" /> that holds the serialized object data about the exception being thrown. </param>
+        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext" /> that contains contextual information about the source or destination. </param>
+        /// <exception cref="T:System.ArgumentNullException">The <paramref name="info" /> parameter is <see langword="null" />. </exception>
+        /// <exception cref="T:System.Runtime.Serialization.SerializationException">The class name is <see langword="null" /> or <see cref="P:System.Exception.HResult" /> is zero (0). </exception>
+        public InvalidRequestEndPointException(SerializationInfo info, StreamingContext context) : base(info, context)
         { }
     }
 }
