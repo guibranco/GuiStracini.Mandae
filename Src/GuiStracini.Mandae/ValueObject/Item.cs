a5da281fcd2a1275cd43004099af012ffbae54b5
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-10-23 00:31:31 +0100

    Remove unused using

--- Src/GuiStracini.Mandae/ValueObject/Item.cs
+++ Src/GuiStracini.Mandae/ValueObject/Item.cs
@@ -13,7 +13,6 @@
 // ***********************************************************************
 
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace GuiStracini.Mandae.ValueObject
 {
