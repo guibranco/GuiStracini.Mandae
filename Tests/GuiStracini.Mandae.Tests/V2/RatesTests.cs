32d53ef8243eb7e124a3fc5a5fef2b12cce3340e
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-12-08 00:55:49 +0000

    Update RatesTests.cs

--- Tests/GuiStracini.Mandae.Tests/V2/RatesTests.cs
+++ Tests/GuiStracini.Mandae.Tests/V2/RatesTests.cs
@@ -56,7 +56,6 @@ public class RatesTests
     /// Validate the get rates asynchronous method.
     /// </summary>
     /// <returns>A Task representing the asynchronous operation.</returns>
-    [Fact]
     public async Task GetRatesAsync()
     {
         var client = new MandaeClient("0b5e2c6410cf0ac087ae7ace111dbd42");
