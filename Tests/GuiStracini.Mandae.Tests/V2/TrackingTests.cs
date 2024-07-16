8df844153b114b4cb8d06b703833b961fd7b4204
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-07-16 17:52:13 +0100

    Disable integration tests  (#197)
    * Update TrackingTests.cs
    
    * Update FlowTests.cs
    
    * Update OrdersTests.cs
    
    * Update deep-source.yml

--- Tests/GuiStracini.Mandae.Tests/V2/TrackingTests.cs
+++ Tests/GuiStracini.Mandae.Tests/V2/TrackingTests.cs
@@ -1,89 +1,89 @@
-// ***********************************************************************
-// Assembly         : GuiStracini.Mandae
-// Author           : Guilherme Branco Stracini
-// Created          : 28/09/2017
-//
-// Last Modified By : Guilherme Branco Stracini
-// Last Modified On : 14/10/2023
-// ***********************************************************************
-// <copyright file="TrackingTest.cs" company="Guilherme Branco Stracini ME">
-//     Copyright © 2017-2023 Guilherme Branco Stracini
-// </copyright>
-// <summary></summary>
-// ***********************************************************************
+// // ***********************************************************************
+// // Assembly         : GuiStracini.Mandae
+// // Author           : Guilherme Branco Stracini
+// // Created          : 28/09/2017
+// //
+// // Last Modified By : Guilherme Branco Stracini
+// // Last Modified On : 14/10/2023
+// // ***********************************************************************
+// // <copyright file="TrackingTest.cs" company="Guilherme Branco Stracini ME">
+// //     Copyright © 2017-2023 Guilherme Branco Stracini
+// // </copyright>
+// // <summary></summary>
+// // ***********************************************************************
 
-using Xunit;
+// using Xunit;
 
-namespace GuiStracini.Mandae.Tests.V2;
+// namespace GuiStracini.Mandae.Tests.V2;
 
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Transport;
+// using System.Linq;
+// using System.Threading;
+// using System.Threading.Tasks;
+// using Transport;
 
-/// <summary>
-/// The tracking test class
-/// </summary>
-public class TrackingTests
-{
-    /// <summary>
-    /// Validates the get tracking method.
-    /// </summary>
-    [SkippableFact]
-    public void GetTracking()
-    {
-        var client = new MandaeClient("0b5e2c6410cf0ac087ae7ace111dbd42");
-        var orderModel = MockOrdersRepository.GetSampleOrderModel();
-        var order = client.CreateOrderCollectRequest(orderModel);
-        var trackingId = AssertOrderResult(order);
-        var tracking = client.GetTracking(trackingId);
-        AssertTrackingResult(tracking, trackingId);
-    }
+// /// <summary>
+// /// The tracking test class
+// /// </summary>
+// public class TrackingTests
+// {
+//     /// <summary>
+//     /// Validates the get tracking method.
+//     /// </summary>
+//     [SkippableFact]
+//     public void GetTracking()
+//     {
+//         var client = new MandaeClient("0b5e2c6410cf0ac087ae7ace111dbd42");
+//         var orderModel = MockOrdersRepository.GetSampleOrderModel();
+//         var order = client.CreateOrderCollectRequest(orderModel);
+//         var trackingId = AssertOrderResult(order);
+//         var tracking = client.GetTracking(trackingId);
+//         AssertTrackingResult(tracking, trackingId);
+//     }
 
-    /// <summary>
-    /// Validates the get tracking asynchronous method.
-    /// </summary>
-    /// <returns>A Task representing the asynchronous operation.</returns>
-    [SkippableFact]
-    public async Task GetTrackingAsync()
-    {
-        await Task.Delay(500);
-        var client = new MandaeClient("0b5e2c6410cf0ac087ae7ace111dbd42");
-        var orderModel = MockOrdersRepository.GetSampleOrderModel();
-        var order = await client.CreateOrderCollectRequestAsync(orderModel, CancellationToken.None);
-        var trackingId = AssertOrderResult(order);
-        var tracking = await client.GetTrackingAsync(trackingId, CancellationToken.None);
-        AssertTrackingResult(tracking, trackingId);
-    }
+//     /// <summary>
+//     /// Validates the get tracking asynchronous method.
+//     /// </summary>
+//     /// <returns>A Task representing the asynchronous operation.</returns>
+//     [SkippableFact]
+//     public async Task GetTrackingAsync()
+//     {
+//         await Task.Delay(500);
+//         var client = new MandaeClient("0b5e2c6410cf0ac087ae7ace111dbd42");
+//         var orderModel = MockOrdersRepository.GetSampleOrderModel();
+//         var order = await client.CreateOrderCollectRequestAsync(orderModel, CancellationToken.None);
+//         var trackingId = AssertOrderResult(order);
+//         var tracking = await client.GetTrackingAsync(trackingId, CancellationToken.None);
+//         AssertTrackingResult(tracking, trackingId);
+//     }
 
-    /// <summary>
-    /// Asserts the order result.
-    /// </summary>
-    /// <param name="order">The order.</param>
-    /// <returns>System.String.</returns>
-    private static string AssertOrderResult(OrderResponse order)
-    {
-        Assert.Null(order.Error);
-        Assert.True(order.Id > 0);
-        var item = order.Items.FirstOrDefault();
-        Skip.If(item == null);
-        Assert.True(item.Id > 0);
-        var trackingId = item.TrackingId;
-        Skip.If(trackingId == null);
-        return trackingId;
-    }
+//     /// <summary>
+//     /// Asserts the order result.
+//     /// </summary>
+//     /// <param name="order">The order.</param>
+//     /// <returns>System.String.</returns>
+//     private static string AssertOrderResult(OrderResponse order)
+//     {
+//         Assert.Null(order.Error);
+//         Assert.True(order.Id > 0);
+//         var item = order.Items.FirstOrDefault();
+//         Skip.If(item == null);
+//         Assert.True(item.Id > 0);
+//         var trackingId = item.TrackingId;
+//         Skip.If(trackingId == null);
+//         return trackingId;
+//     }
 
-    /// <summary>
-    /// Asserts the tracking result.
-    /// </summary>
-    /// <param name="tracking">The tracking.</param>
-    /// <param name="trackingId">The tracking identifier.</param>
-    private static void AssertTrackingResult(TrackingResponse tracking, string trackingId)
-    {
-        Assert.Null(tracking.Error);
-        Assert.Equal(trackingId, tracking.TrackingCode);
-        Assert.Null(tracking.CarrierName);
-        Assert.Null(tracking.CarrierCode);
-        Assert.False(tracking.Events.Any());
-    }
-}
+//     /// <summary>
+//     /// Asserts the tracking result.
+//     /// </summary>
+//     /// <param name="tracking">The tracking.</param>
+//     /// <param name="trackingId">The tracking identifier.</param>
+//     private static void AssertTrackingResult(TrackingResponse tracking, string trackingId)
+//     {
+//         Assert.Null(tracking.Error);
+//         Assert.Equal(trackingId, tracking.TrackingCode);
+//         Assert.Null(tracking.CarrierName);
+//         Assert.Null(tracking.CarrierCode);
+//         Assert.False(tracking.Events.Any());
+//     }
+// }
